06509414b70e9b6f74bcda98a5f535f69e1b065c
Quentin Renaud <quentin.renaud@epitech.eu>
2019-06-20 05:56:48 -0700

    [FIX] OTP 19.0 can't be build with SSH

---
+++ kbrw/lib/server/jsonloader.ex
@@ -0,0 +1,9 @@
+defmodule KBRW.Jsonloader do
+
+	@doc """
+	Insert Json data in database.
+	"""
+	def load_to_database(_database, _json_file) do
+		{:ok}
+	end
+end
--- kbrw/mix.exs
+++ kbrw/mix.exs
@@ -4,7 +4,7 @@ defmodule Kbrw.Mixfile do
   def project do
     [app: :kbrw,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -30,6 +30,6 @@ defmodule Kbrw.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:poison, "~> 3.1"}]
   end
 end
