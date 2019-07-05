ded110506761a239e9caeffe0c7cc4b8b76148ca
Quentin Renaud <quentin.renaud@epitech.eu>
2019-07-05 10:44:07 -0700

    [ADD] Multiple routes

--- kbrw/lib/server/router.ex
+++ kbrw/lib/server/router.ex
@@ -7,6 +7,10 @@
 
     def call(conn, _opts) do
       put_resp_content_type(conn, "application/json")
-      send_resp(conn, 200, "Hello world")
+      cond do
+      	conn.request_path == "/me" -> send_resp(conn, 200, "I am The First, The One, Le Geant Plug Vert, Le Grand Plug, Le Plug Cosmique.")	
+      	conn.request_path == "/" -> send_resp(conn, 200, "Welcome to the new world of Plugs!")     	
+      	true -> send_resp(conn, 404, "Go away, you are not welcome here.")
+      end
     end
   end
